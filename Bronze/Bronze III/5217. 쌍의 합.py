7546ae53b105a55a46a510a5a274b736a697e854
juuuun_b  <gudwns5220@gmail.com>
2021-02-03 02:17:54 +0900

    Update 5217. 쌍의 합.py

--- Bronze/Bronze III/5217. 쌍의 합.py
+++ Bronze/Bronze III/5217. 쌍의 합.py
@@ -4,3 +4,12 @@ n = int(stdin.readline().rstrip())
 
 for step in range(n):
     t = int(stdin.readline().rstrip())
+
+    arr = []
+
+    for x in range(1, t):
+        for y in range(x, t):
+            if x + y == t and x != y:
+                arr.append([x, y])
+
+    

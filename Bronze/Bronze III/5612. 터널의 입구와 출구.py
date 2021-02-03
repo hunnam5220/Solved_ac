d35887436f2c270876f8d8caf829855fbc03c57b
juuuun_b  <gudwns5220@gmail.com>
2021-02-03 17:25:55 +0900

    Update 5612. 터널의 입구와 출구.py

--- Bronze/Bronze III/5612. 터널의 입구와 출구.py
+++ Bronze/Bronze III/5612. 터널의 입구와 출구.py
@@ -2,20 +2,24 @@ from sys import stdin
 
 n = int(stdin.readline().rstrip())
 m = int(stdin.readline().rstrip())
-t = 0
-result = 0
+result, t = 0, 0
 
 for step in range(n):
     a, b = map(int, stdin.readline().rstrip().split())
     if step == 0:
-        if t < (m - a + b):
-            t = (m - a + b)
-    else:
-        tmp = t - a + b
-        if t < tmp:
-            t = t - a + b
+        if m + a - b < 0:
+            result = 0
+            break
 
-    if result < t:
+        elif t < (m + a - b):
+            t = (m + a - b)
+    else:
+        t += a - b
+        if t < 0:
+            result = 0
+            break
+
+    if t > result:
         result = t
 
 print(result)

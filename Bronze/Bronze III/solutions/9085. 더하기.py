598971d021cb8db3e987a5185533b18501c32c61
juuuun_b  <gudwns5220@gmail.com>
2021-02-08 21:48:52 +0900

    Create 9085. 더하기.py

---
+++ Bronze/Bronze III/solutions/9085. 더하기.py
@@ -0,0 +1,5 @@
+from sys import stdin
+
+for step in range(int(stdin.readline().rstrip())):
+    num = int(stdin.readline().rstrip())
+    print(sum(list(map(int, stdin.readline().rstrip().split()))))

38cebc02131584eea00f07beee541876f8a886b4
juuuun_b  <gudwns5220@gmail.com>
2021-02-11 01:55:10 +0900

    Update 9493. 길면 기차, 기차는 빨라, 빠른 것은 비행기.py

--- Bronze/Bronze III/9493. 길면 기차, 기차는 빨라, 빠른 것은 비행기.py
+++ Bronze/Bronze III/9493. 길면 기차, 기차는 빨라, 빠른 것은 비행기.py
@@ -1,2 +1,12 @@
 from sys import stdin
 
+while 1:
+    m, a, b = map(int, stdin.readline().rstrip().split())
+    m, a, b = m * 1000, a * 1000 / 3600, b * 1000 / 3600
+
+    if m == a == b == 0:
+        break
+
+    else:
+        print((m / a))
+        print((m / b))

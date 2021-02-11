b4279b844180a3326f40f45590bca52b6d72bb64
juuuun_b  <gudwns5220@gmail.com>
2021-02-11 17:01:27 +0900

    Create 9610. 사분면.py

---
+++ Bronze/Bronze III/solutions/9610. 사분면.py
@@ -0,0 +1,26 @@
+from sys import stdin
+
+q1, q2, q3, q4, axis = 0, 0, 0, 0, 0
+for step in range(int(stdin.readline().rstrip())):
+    x, y = map(int, stdin.readline().rstrip().split())
+
+    if x == 0 or y == 0:
+        axis += 1
+
+    elif x < 0 < y:
+        q2 += 1
+
+    elif y < 0 < x:
+        q4 += 1
+
+    elif x > 0 and y > 0:
+        q1 += 1
+
+    elif x < 0 and y < 0:
+        q3 += 1
+
+print('Q1:', q1)
+print('Q2:', q2)
+print('Q3:', q3)
+print('Q4:', q4)
+print('AXIS:', axis)

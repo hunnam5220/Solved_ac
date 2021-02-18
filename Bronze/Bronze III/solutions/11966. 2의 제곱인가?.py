3f1d75a43d5bc708e521e1a02093eab1f5fbaf9b
juuuun_b  <gudwns5220@gmail.com>
2021-02-19 02:20:50 +0900

    Update 11966. 2의 제곱인가?.py

--- Bronze/Bronze III/solutions/11966. 2의 제곱인가?.py
+++ Bronze/Bronze III/solutions/11966. 2의 제곱인가?.py
@@ -1,2 +1,16 @@
 from sys import stdin
 
+n = int(stdin.readline().rstrip())
+a = 0
+
+while 1:
+    if n == 1:
+        print(1)
+        break
+
+    elif 1 < n < 2:
+        print(0)
+        break
+
+    else:
+        n /= 2

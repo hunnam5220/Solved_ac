7489ef6412570592617c4c05c2b91ce9579cbe28
juuuun_b  <gudwns5220@gmail.com>
2021-02-25 19:54:44 +0900

    Create 14920. 3n+1 수열.py

---
+++ Bronze/Bronze III/solutions/14920. 3n+1 수열.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+cn = int(stdin.readline().rstrip())
+n = 1
+
+if cn != 1:
+    while 1:
+        if cn % 2 == 0:
+            cn //= 2
+
+        else:
+            cn = 3 * cn + 1
+
+        n += 1
+
+            print(n)
+            break
+else:
+    print(1)

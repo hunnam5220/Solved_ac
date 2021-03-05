c568bea08338a6fd83b45b1c490861533117420f
juuuun_b  <gudwns5220@gmail.com>
2021-03-05 19:59:14 +0900

    i am idot...ㅠㅠ

---
+++ !solved_gold_4_1987. 알파벳.py
@@ -0,0 +1,30 @@
+from sys import stdin, setrecursionlimit
+setrecursionlimit(1000)
+
+r, c = map(int, stdin.readline().rstrip().split())
+
+arr = []
+visited = []
+for step in range(r):
+    arr.append(list(stdin.readline().rstrip()))
+
+
+def dfs(x, y, visited):
+    if x < 0 or y < 0 or x >= r or y >= c:
+        return False
+
+    if arr[x][y] not in visited:
+        visited.append(arr[x][y])
+        dfs(x, y - 1, visited)
+        dfs(x - 1, y, visited)
+        dfs(x + 1, y, visited)
+        dfs(x, y + 1, visited)
+        return True
+
+    visited.pop()
+    return False
+
+
+result = 0
+dfs(0, 0, visited)
+print(len(visited))

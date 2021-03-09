a2f6773b73213a8ca26929788c447bdff7006a6a
juuuun_b  <gudwns5220@gmail.com>
2021-03-09 18:30:32 +0900

    Update !solved_silver_2_11725. 트리의 부모 찾기.py

--- !solved_silver_2_11725. 트리의 부모 찾기.py
+++ !solved_silver_2_11725. 트리의 부모 찾기.py
@@ -1,3 +1,5 @@
+# BFS
+
 from sys import stdin, setrecursionlimit
 setrecursionlimit(1000000)
 
@@ -12,17 +14,10 @@ for step in range(n - 1):
     arr[y - 1].append(x)
 
 
-def dfs(arr, v, visited):
-    visited[v - 1] = True
-    result.append(v)
+def dfs(arr, v, result):
+    stack = [v]
 
-    for step in arr[v - 1]:
-        if not visited[step - 1]:
-            dfs(arr, step, visited)
+    while stack:
+        break
 
-
-visited = [False] * n
-dfs(arr, 1, visited)
-
-for step in range(2, n + 1):
-    print(result[result.index(step) - 1])
+dfs(arr, 1, result)

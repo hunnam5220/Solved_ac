5f281d1235782fedfbafe37b2f25bc9229468d5b
juuuun_b  <gudwns5220@gmail.com>
2021-04-14 23:49:18 +0900

    Create 1932. 정수 삼각형.py

---
+++ 02. Silver/Silver I/1932. 정수 삼각형.py
@@ -0,0 +1,35 @@
+from sys import stdin
+
+n = int(stdin.readline().rstrip())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, stdin.readline().rstrip().split())))
+
+dp = [item[:] for item in arr]
+start = arr[0][0]
+
+for i in range(1, n):
+    for j in range(len(arr[i])):
+        if i == 1:
+            dp[i][j] = dp[i][j] + dp[i - 1][0]
+
+        elif j == 0:
+            dp[i][j] = dp[i][j] + dp[i - 1][0]
+
+        elif j == len(arr[i]) - 1:
+            dp[i][j] = dp[i][j] + dp[i - 1][j - 1]
+
+        else:
+            dp[i][j] = max(dp[i - 1][j] + dp[i][j], dp[i - 1][j - 1] + dp[i][j])
+
+print(max(dp[-1]))
+
+
+"""
+5
+1
+1 1
+1 0 1
+1 0 0 1
+1 0 100 0 1
+"""

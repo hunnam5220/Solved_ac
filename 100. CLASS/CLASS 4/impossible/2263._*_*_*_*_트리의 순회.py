5061e04876ad158e0dc99b3e702631269e4d986d
juuuun_b  <gudwns5220@gmail.com>
2021-10-31 16:34:16 +0900

    Update 2263._*_*_*_*_트리의 순회.py

--- 100. CLASS/CLASS 4/impossible/2263._*_*_*_*_트리의 순회.py
+++ 100. CLASS/CLASS 4/impossible/2263._*_*_*_*_트리의 순회.py
@@ -11,17 +11,24 @@ def solve(in_s, in_e, post_s, post_e):
     if (in_s > in_e) or (post_s > post_e):
         return
 
+    # postorder 에서 post_e 위치에 있는 것이 서브트리의 루트노드
     parents = postorder[post_e]
     print(parents, end=" ")
 
+    # inorder 위치에 해당하는 위치들
     left = pos[parents] - in_s
     right = in_e - pos[parents]
 
+    # 왼쪽 서브트리 구간에 해당하는 index
     solve(in_s, in_s + left - 1, post_s, post_s + left - 1)
+
+    # 오른쪽 서브트리 구간에 해당하는 index
     solve(in_e - right + 1, in_e, post_e - right, post_e - 1)
 
 
 pos = [0 for _ in range(n + 1)]
+
+# inorder 숫자들의 위치 기록
 for i in range(n):
     pos[inorder[i]] = i
 

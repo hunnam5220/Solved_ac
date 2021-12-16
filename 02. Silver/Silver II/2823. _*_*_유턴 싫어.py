81ff20738992294d9e05b23d85689d2a9738ab41
juuuun_b  <gudwns5220@gmail.com>
2021-12-16 22:44:57 +0900

    Update 2823. _*_*_유턴 싫어.py

--- 02. Silver/Silver II/2823. _*_*_유턴 싫어.py
+++ 02. Silver/Silver II/2823. _*_*_유턴 싫어.py
@@ -3,9 +3,13 @@ from sys import stdin
 
 r, c = map(int, stdin.readline().split())
 arr, roads, dx, dy = [], [], [1, 0, 0, -1], [0, 1, -1, 0]
+
+# row의 개수만큼 데이터를 입력받아 arr에 저장
 for i in range(r):
     data = list(stdin.readline().rstrip())
     arr.append(data)
+
+    # .에 해당되는 좌표를 길인지 아닌지 구별해 roads에 저장
     for j in range(c):
         if data[j] == '.':
             roads.append((i, j))
@@ -18,7 +22,10 @@ def solve(x, y):
 
     while q:
         x, y = q.popleft()
+        # . 하나당 길이 두 개 이상 있어야 한다.
+        # 그 조건을 알아낼 수 있는 cnt를 만든다.
         cnt = 0
+
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if -1 < nx < r and -1 < ny < c:
@@ -29,6 +36,7 @@ def solve(x, y):
                         visited.add((nx, ny))
                         q.append((nx, ny))
 
+        # 길이 두개 이하라면 1을 return
         if cnt < 2:
             return 1
 
@@ -36,6 +44,8 @@ def solve(x, y):
 
 
 for x, y in roads:
+    # 유턴을 하지 않고 제자리로 돌아올 수 있다면
+    # 0 출력하고 종료
     if solve(x, y) == 0:
         print(0)
         exit()
